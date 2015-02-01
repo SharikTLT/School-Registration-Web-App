425a5531f76f5caf256b4d323704d5a7f7e380f2
calen-legaspi <calen@orangeandbronze.com>
2015-02-01 19:02:10 +0800

    Fixes #23.

--- src/test/java/spec/StudentEnlistsFixture.java
+++ src/test/java/spec/StudentEnlistsFixture.java
@@ -1,5 +1,9 @@
 package spec;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Set;
@@ -7,8 +11,12 @@ import java.util.Set;
 import org.concordion.integration.junit4.ConcordionRunner;
 import org.dbunit.DBTestCase;
 import org.dbunit.PropertiesBasedJdbcDatabaseTester;
+import org.dbunit.database.DatabaseConnection;
+import org.dbunit.database.IDatabaseConnection;
 import org.dbunit.dataset.IDataSet;
+import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.operation.DatabaseOperation;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
@@ -19,37 +27,23 @@ import com.orangeandbronze.test.IntegrationTest;
 
 @Category(IntegrationTest.class)
 @RunWith(ConcordionRunner.class)
-public class StudentEnlistsFixture extends DBTestCase {
+public class StudentEnlistsFixture {
 
-	private final String datasetFilename = "StudentEnlistsFixture.xml";
-
-	@Override
-	protected IDataSet getDataSet() throws Exception {
+	public Collection enlistAndCheckIfSaved(int studentNumber, String sectionId) throws Exception {
+		Connection jdbcConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/school_registration?sessionVariables=FOREIGN_KEY_CHECKS=0", "root", "");
+		IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
 		FlatXmlDataSetBuilder builder = new FlatXmlDataSetBuilder();
-		builder.setDtdMetadata(false);
-		return builder.build(getClass().getResourceAsStream(datasetFilename));
-	}
-
-	// TO DO: Remove mocks and have this call actual database
-	public Collection enlistAndCheckIfSaved(int studentNumber, String sectionId) {
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
-				"jdbc:mysql://localhost:3306/school_registration?sessionVariables=FOREIGN_KEY_CHECKS=0");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, "root");
-		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, "");
+		builder.setDtdMetadata(false);		 
+		IDataSet dataSet = builder.build(getClass().getResourceAsStream("StudentEnlistsFixture.xml"));
+		try {
+			DatabaseOperation.CLEAN_INSERT.execute(connection, dataSet);
+		} finally {
+			connection.close();
+		}
 
 		EnlistService service = new EnlistService();
-
 		EnlistmentResult result = service.enlistSections(studentNumber, sectionId);
-
 		Set<Section> resultSections = result.getSuccessfullyEnlisted();
-		
-//		for (Section sec : resultSections) {
-//			if (sec.getSectionNumber().equals(sectionId)) {
-//				return true;
-//			}
-//		}
-		System.out.println(resultSections);
-		return new ArrayList(resultSections);		// TODO: Have this return collection instead.
+		return new ArrayList(resultSections); 
 	}
 }
