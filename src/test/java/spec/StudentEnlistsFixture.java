06316d52df14189477c301e35f5771dfe041a023
calen-legaspi <calen@orangeandbronze.com>
2015-02-01 18:29:04 +0800

    Concordion test now uses verifyRows instead of assertTrue.

--- src/test/java/spec/StudentEnlistsFixture.java
+++ src/test/java/spec/StudentEnlistsFixture.java
@@ -1,8 +1,7 @@
 package spec;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
-
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Set;
 
 import org.concordion.integration.junit4.ConcordionRunner;
@@ -13,13 +12,7 @@ import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import com.orangeandbronze.schoolreg.dao.EnrollmentDao;
-import com.orangeandbronze.schoolreg.dao.SectionDao;
-import com.orangeandbronze.schoolreg.dao.StudentDao;
-import com.orangeandbronze.schoolreg.domain.Enrollment;
 import com.orangeandbronze.schoolreg.domain.Section;
-import com.orangeandbronze.schoolreg.domain.Student;
-import com.orangeandbronze.schoolreg.domain.Subject;
 import com.orangeandbronze.schoolreg.service.EnlistService;
 import com.orangeandbronze.schoolreg.service.EnlistService.EnlistmentResult;
 import com.orangeandbronze.test.IntegrationTest;
@@ -38,7 +31,7 @@ public class StudentEnlistsFixture extends DBTestCase {
 	}
 
 	// TO DO: Remove mocks and have this call actual database
-	public boolean enlistAndCheckIfSaved(int studentNumber, String sectionId) {
+	public Collection enlistAndCheckIfSaved(int studentNumber, String sectionId) {
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, "com.mysql.jdbc.Driver");
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL,
 				"jdbc:mysql://localhost:3306/school_registration?sessionVariables=FOREIGN_KEY_CHECKS=0");
@@ -51,12 +44,12 @@ public class StudentEnlistsFixture extends DBTestCase {
 
 		Set<Section> resultSections = result.getSuccessfullyEnlisted();
 		
-		for (Section sec : resultSections) {
-			if (sec.getSectionNumber().equals(sectionId)) {
-				return true;
-			}
-		}
-
-		return false;
+//		for (Section sec : resultSections) {
+//			if (sec.getSectionNumber().equals(sectionId)) {
+//				return true;
+//			}
+//		}
+		System.out.println(resultSections);
+		return new ArrayList(resultSections);		// TODO: Have this return collection instead.
 	}
 }
